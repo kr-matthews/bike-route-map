57e4456c53f965d3fdde8933ea76e0c20654b1a5
Kevin Matthews <kvnrmatthews@gmail.com>
2023-06-14 20:12:08 -0700

    Add some placeholder data

---
+++ src/data/index.js
@@ -0,0 +1,38 @@
+const BIKE_ROUTE_DATA = [
+  { name: "Haro", direction: "eastbound", positions: [], video: "" },
+  { name: "Haro", direction: "westbound", positions: [], video: "" },
+  { name: "10th", direction: "eastbound", positions: [], video: "" },
+  { name: "10th", direction: "westbound", positions: [], video: "" },
+  {
+    name: "Midtown",
+    segment: "Vancouver",
+    direction: "eastbound",
+    positions: [],
+    video: "",
+  },
+  {
+    name: "Midtown",
+    segment: "Vancouver",
+    direction: "westbound",
+    positions: [],
+    video: "",
+  },
+  { name: "Ontario", direction: "northbound", positions: [], video: "" },
+  { name: "Ontario", direction: "southbound", positions: [], video: "" },
+  {
+    name: "Seaside",
+    segment: "Stanley Park",
+    direction: "clockwise",
+    positions: [],
+    video: "",
+  },
+  {
+    name: "Seaside",
+    segment: "Stanley Park",
+    direction: "counter-clockwise",
+    positions: [],
+    video: "",
+  },
+];
+
+export { BIKE_ROUTE_DATA };
