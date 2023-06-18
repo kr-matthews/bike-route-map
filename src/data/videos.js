357d33e558f4bd70464f2ec689a5b4f704a80ccc
Kevin Matthews <kvnrmatthews@gmail.com>
2023-06-18 16:56:20 -0700

    Refactor data to routes->legs->segments
    with emphasize on testing Cambie Bridge.

---
+++ src/data/videos.js
@@ -0,0 +1,32 @@
+const VIDEOS = {
+  tenthWb: "R9Im8PUzSlM",
+  tenthEb: "wgLoXc7ydDA",
+  hillcrestEb: "gwZnVGbA4x4",
+  hillcrestWb: "wnFFioj6OVA",
+  carrallNb: "1KNO98tWduE",
+  alexanderEb: "5pjlcdGKa10",
+  powellEb: "vTtdPgHQCp0",
+  pandoraEb: "-vNVdK9fnMU",
+  cambieBridgeSmithNb: "-vNVdK9fnMU",
+  cambieBridgeNelsonSb: "FWN1apBAy3g",
+  sixtySeventhPlusWb: "z5GzRN2_iNc",
+  masumiMitsuiEb: "QVK6E_hmQFc",
+  offBroadwayWb: "7nKl_moRiog",
+  fourthEb: "19BLtcs-t7U",
+  thirdEb: "B5KPg3I32Ws",
+  midtownVancouverEb: "3qGbnTOYyL0",
+  twentySecondEb: "4vjaCs_VJTg",
+  comoxHelmckenWb: "PgJUF6B4BMQ",
+  comoxHelmckenEb: "_u16UGWH4VQ",
+  haroWb: "gdsFxEtuepY",
+  lionsGateBridgeNb: "KbhPPsdOzIE",
+  lionsGateBridgeSb: "eHio3HMRIMw",
+};
+
+const urlPrefix = "https://www.youtube.com/embed/";
+
+Object.entries(VIDEOS).forEach(
+  ([route, id]) => (VIDEOS[route] = `${urlPrefix}${id}`)
+);
+
+export { VIDEOS };
