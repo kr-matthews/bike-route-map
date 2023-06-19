da1cd7aefbd2271d7148dad1a810d7f4b4461ccc
Kevin Matthews <kvnrmatthews@gmail.com>
2023-06-18 21:58:17 -0700

    Add more data - Haro, Smithe

--- src/data/index.js
+++ src/data/index.js
@@ -4,6 +4,7 @@ import { CAMBIE_BRIDGE } from "./cambieBridge";
 import { HARO } from "./haro";
 import { MASUMI_MITSUI_GREENWAY } from "./masumiMitsuiGreenway";
 import { MIDTOWN } from "./midtown";
+import { SMITHE } from "./smithe";
 
 // convention: bi-directional paths are stored northbound, eastbound, or clockwise
 export const BIKE_ROUTE_DATA = [
@@ -11,4 +12,5 @@ export const BIKE_ROUTE_DATA = [
   HARO,
   MASUMI_MITSUI_GREENWAY,
   CAMBIE_BRIDGE,
+  SMITHE,
 ];
