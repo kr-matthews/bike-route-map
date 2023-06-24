e3374a0d55f2be009a87262af796d7f99994aca8
Kevin Matthews <kvnrmatthews@gmail.com>
2023-06-24 12:01:45 -0700

    Decorator arrows partially interactive (hover, click)

--- src/components/DirectedPolyline.jsx
+++ src/components/DirectedPolyline.jsx
@@ -1,31 +1,61 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Polyline, useMap } from "react-leaflet";
 import L from "leaflet";
 import "leaflet-polylinedecorator";
 
-// TODO: refine
-const arrow = [
-  {
-    offset: "3%",
-    repeat: 80,
-    symbol: L.Symbol.arrowHead({
-      pixelSize: 10,
-      polygon: false,
-      pathOptions: { stroke: true, color: "blue" },
-    }),
-  },
-];
+// const arrow = [
+//   {
+//     offset: "3%",
+//     repeat: 80,
+//     symbol: L.Symbol.arrowHead({
+//       pixelSize: 10,
+//       polygon: false,
+//       pathOptions: { stroke: true, color: "blue" },
+//     }),
+//   },
+// ];
 
 /** A Polyline, but with arrows indicating the direction. */
 export default function DirectedPolyline(props) {
   const map = useMap();
   const polylineRef = useRef();
+  const [decorator, setDecorator] = useState();
+  // const isZoomedIn = map.getZoom() > 15;
+
+  // useEffect(function addZoomListener() {
+  //   map.addEventListener({ zoomend: (zoom) => console.log(zoom.target._zoom) });
+  // }, []);
 
   useEffect(
-    function addArrows() {
-      L.polylineDecorator(polylineRef.current, { patterns: arrow }).addTo(map);
+    function addDecorator() {
+      const decorator = L.polylineDecorator(polylineRef.current, {
+        patterns: [],
+      });
+      decorator.addTo(map);
+      decorator.addEventListener(props.eventHandlers);
+      setDecorator(decorator);
     },
-    [map]
+    // eslint-disable-next-line
+    []
+  );
+
+  useEffect(
+    function updateArrows() {
+      const arrow = {
+        offset: "3%",
+        repeat: 90,
+        symbol: L.Symbol.arrowHead({
+          pixelSize: 14,
+          polygon: true,
+          pathOptions: { ...props.pathOptions },
+        }),
+      };
+
+      if (decorator) {
+        decorator.setPatterns([arrow]);
+      }
+    },
+    [decorator, props.pathOptions]
   );
 
   return <Polyline ref={polylineRef} {...props} />;
