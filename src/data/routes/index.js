81c62aefd2a055f46e467259c5b149881d73534c
Kevin Matthews <kvnrmatthews@gmail.com>
2023-07-09 20:26:56 -0700

    Add most remaining van routes to ROUTES
    excluding some ambiguous ones like seaside (bypass).

--- src/data/routes/index.js
+++ src/data/routes/index.js
@@ -1,12 +1,18 @@
 import { VIDEOS } from "../videos";
 
-// !! add routes for existing videos
 // !! add missing un-named segments for existing routes
+// TODO: separate by city? add cities: [...] property?
+// TODO: add remaining Vancouver routes (the confusing ones, like seaside (bypass)...)
 
 export const ROUTES = {
+  // ? are these legs of seaside bypass?
   third: {
     name: "3rd",
     legs: [{ videos: { eastbound: VIDEOS.thirdEb, westbound: null } }],
+  },
+  fourth: {
+    name: "4th",
+    legs: [{ videos: { eastbound: VIDEOS.fourthEb } }],
   },
   tenth: {
     name: "10th",
@@ -14,9 +20,37 @@ export const ROUTES = {
       { videos: { eastbound: VIDEOS.tenthEb, westbound: VIDEOS.tenthWb } },
     ],
   },
+  fourteenth: {
+    name: "14th",
+    legs: [],
+  },
+  twentySecond: {
+    name: "22nd",
+    legs: [{ videos: { westbound: VIDEOS.twentySecondEb } }],
+  },
   twentyNinth: {
     name: "29th",
     legs: [{ videos: {} }],
+  },
+  fortyFifth: {
+    name: "45th",
+    legs: [],
+  },
+  sixtyThirdSixtyFourth: {
+    name: "63rd / 64th",
+    legs: [{ videos: { westbound: VIDEOS.sixtySeventhPlusWb } }],
+  },
+  adanac: {
+    name: "Adanac",
+    legs: [],
+  },
+  alberni: {
+    name: "Alberni",
+    legs: [],
+  },
+  alder: {
+    name: "Alder",
+    legs: [],
   },
   alexander: {
     name: "Alexander",
@@ -27,6 +61,44 @@ export const ROUTES = {
           westbound: null,
         },
       },
+    ],
+  },
+  arbutus: {
+    name: "Arbutus Greenway",
+    legs: [],
+  },
+  balaclava: {
+    name: "Balaclava",
+    legs: [],
+  },
+  beatty: {
+    name: "Beatty",
+    legs: [],
+  },
+  blanca: {
+    name: "Blanca",
+    legs: [],
+  },
+  burnaby: {
+    name: "Burnaby",
+    legs: [],
+  },
+  burrard: {
+    name: "Burrard",
+    legs: [],
+  },
+  bute: {
+    name: "Bute",
+    legs: [],
+  },
+  cambie: {
+    name: "Cambie",
+    legs: [
+      {
+        name: "Marine Drive Station Separated Path",
+        videos: { northbound: VIDEOS.cambieSeparatedNb },
+      },
+      { name: "On-Street", videos: {} },
     ],
   },
   cambieBridge: {
@@ -40,11 +112,38 @@ export const ROUTES = {
       },
     ],
   },
+  cardero: {
+    name: "Cardero",
+    legs: [],
+  },
   carrall: {
     name: "Carrall",
     legs: [
       {
         videos: { northbound: VIDEOS.carrallNb, southbound: null },
+      },
+    ],
+  },
+  cassiar: {
+    name: "Cassiar",
+    legs: [],
+  },
+  chilco: {
+    name: "Chilco",
+    legs: [],
+  },
+  columbia: {
+    name: "Columbia",
+    legs: [],
+  },
+  comoxHelmcken: {
+    name: "Comox Helmcken",
+    legs: [
+      {
+        videos: {
+          eastbound: VIDEOS.comoxHelmckenEb,
+          westbound: VIDEOS.comoxHelmckenWb,
+        },
       },
     ],
   },
@@ -55,9 +154,48 @@ export const ROUTES = {
       { name: "Burnaby", videos: {} },
     ],
   },
+  cypress: {
+    name: "Cypress",
+    legs: [],
+  },
+  dumfries: {
+    name: "Dumfries",
+    legs: [],
+  },
+  dunbar: {
+    name: "Dunbar",
+    legs: [],
+  },
+  dunsmuir: {
+    name: "Dunsmuir",
+    legs: [],
+  },
+  gardenCity: {
+    name: "Garden City",
+    legs: [
+      {
+        videos: {
+          northbound: VIDEOS.gardenCityNb,
+          southbound: VIDEOS.gardenCitySb,
+        },
+      },
+    ],
+  },
+  gladstone: {
+    name: "Gladstone",
+    legs: [],
+  },
   haro: {
     name: "Haro",
     legs: [{ videos: { westbound: VIDEOS.haroWb } }],
+  },
+  heather: {
+    name: "Heather",
+    legs: [],
+  },
+  highbury: {
+    name: "Highbury",
+    legs: [],
   },
   hillcrest: {
     name: "Hillcrest",
@@ -69,6 +207,38 @@ export const ROUTES = {
         },
       },
     ],
+  },
+  hornby: {
+    name: "Hornby",
+    legs: [],
+  },
+  inverness: {
+    name: "Inverness",
+    legs: [],
+  },
+  keefer: {
+    name: "Keefer",
+    legs: [],
+  },
+  kent: {
+    name: "Kent",
+    legs: [],
+  },
+  kerr: {
+    name: "Kerr",
+    legs: [],
+  },
+  killarney: {
+    name: "Killarney",
+    legs: [],
+  },
+  kingEdward: {
+    name: "King Edward",
+    legs: [],
+  },
+  lakewood: {
+    name: "Lakewood",
+    legs: [],
   },
   lionsGate: {
     name: "Lions Gate Bridge",
@@ -115,21 +285,80 @@ export const ROUTES = {
     name: "Mosaic",
     legs: [{ videos: {} }],
   },
+  nanaimo: {
+    name: "Nanaimo",
+    legs: [],
+  },
+  nelson: {
+    name: "Nelson",
+    legs: [{ videos: { eastbound: VIDEOS.cambieBridgeNelsonSb } }],
+  },
+  northArmBridge: {
+    name: "North Arm Bridge (aka Canada Line Bridge)",
+    legs: [
+      {
+        videos: {
+          northbound: VIDEOS.northArmBridgeNb,
+          southbound: VIDEOS.northArmBridgeSb,
+        },
+      },
+    ],
+  },
   offBroadway: {
     name: "Off-Broadway",
     legs: [{ videos: { eastbound: null, westbound: VIDEOS.offBroadwayWb } }],
+  },
+  ontario: {
+    name: "Ontario",
+    legs: [],
+  },
+  pandora: {
+    name: "Pandora",
+    legs: [{ videos: { eastbound: VIDEOS.pandoraEb } }],
   },
   powell: {
     name: "Powell",
     legs: [{ videos: { eastbound: VIDEOS.powellEb, westbound: null } }],
   },
+  princeEdward: {
+    name: "Prince Edward",
+    legs: [],
+  },
+  richards: {
+    name: "Richards",
+    legs: [],
+  },
+  ridgeway: {
+    name: "Ridgeway",
+    legs: [],
+  },
+  rupert: {
+    name: "Rupert",
+    legs: [],
+  },
   smithe: {
     name: "Smithe",
     legs: [{ videos: { westbound: VIDEOS.cambieBridgeSmithNb } }],
   },
+  sunrise: {
+    name: "Sunrise",
+    legs: [],
+  },
   valley: {
     name: "Valley",
     legs: [{ videos: { northbound: null, southbound: VIDEOS.valleySb } }],
+  },
+  walesDuchess: {
+    name: "Wales Duchess",
+    legs: [],
+  },
+  windermere: {
+    name: "Windermere",
+    legs: [],
+  },
+  windsor: {
+    name: "Windsor",
+    legs: [],
   },
   yukon: {
     name: "Yukon",
