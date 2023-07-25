b0c354a92567a995eb6e90b10550355cbeff13a8
Kevin Matthews <kvnrmatthews@gmail.com>
2023-07-24 18:34:49 -0700

    Different colour for uni-direction video segments

--- src/utils/pathOptions.js
+++ src/utils/pathOptions.js
@@ -2,7 +2,8 @@ const NARROW_WEIGHT = 6;
 const WIDE_WEIGHT = 9;
 const DIM_OPACITY = 0.5;
 const FULL_OPACITY = 1;
-const VIDEO_COLOUR = "Purple";
+const VIDEO_BIDIRECTIONAL_COLOUR = "Purple";
+const VIDEO_UNIDIRECTIONAL_COLOUR = "Magenta";
 const BIDIRECTIONAL_COLOUR = "DarkGreen";
 const UNIDIRECTIONAL_COLOUR = "Blue";
 const DASH_PATTERN = "6, 10";
@@ -20,11 +21,15 @@ export function createPathOptions(
 
   const nonVideoColour =
     directions?.length === 1 ? UNIDIRECTIONAL_COLOUR : BIDIRECTIONAL_COLOUR;
+  const videoColour =
+    directions?.length === 1
+      ? VIDEO_UNIDIRECTIONAL_COLOUR
+      : VIDEO_BIDIRECTIONAL_COLOUR;
 
   if (hideUnlessVideo && !hasActiveVideo) return { weight: 0, opacity: 0 };
 
   return {
-    color: hasActiveVideo ? VIDEO_COLOUR : nonVideoColour,
+    color: hasActiveVideo ? videoColour : nonVideoColour,
     weight: isHighlighted || hasActiveVideo ? WIDE_WEIGHT : NARROW_WEIGHT,
     opacity: isSelected || isNoneSelected ? FULL_OPACITY : DIM_OPACITY,
     dashArray: isUnofficial || isUncomfortable ? DASH_PATTERN : undefined,
