2f2e84c22c952991b856b57f90f2616cbc2e7a09
Kevin Matthews <kvnrmatthews@gmail.com>
2023-08-08 21:21:11 -0700

    Use lighter colours intead of partial opacity
    and change colours, and don't change all other routes when selecting a
    route.

--- src/utils/pathOptions.js
+++ src/utils/pathOptions.js
@@ -1,26 +1,35 @@
 const NARROW_WEIGHT = 6;
 const WIDE_WEIGHT = 9;
-const DIM_OPACITY = 0.5;
-const FULL_OPACITY = 1;
+// const DIM_OPACITY = 0.5;
+// const FULL_OPACITY = 1;
 const VIDEO_BIDIRECTIONAL_COLOUR = "Purple";
-const VIDEO_UNIDIRECTIONAL_COLOUR = "Magenta";
-const BIDIRECTIONAL_COLOUR = "DarkGreen";
-const UNIDIRECTIONAL_COLOUR = "Blue";
+const VIDEO_UNIDIRECTIONAL_COLOUR = "Purple";
+const BIDIRECTIONAL_COLOUR_FULL = "DarkGreen";
+const BIDIRECTIONAL_COLOUR_LIGHT = "#009E60";
+const UNIDIRECTIONAL_COLOUR_FULL = "Blue";
+const UNIDIRECTIONAL_COLOUR_LIGHT = "#6495ED";
 const DASH_PATTERN = "6, 10";
 
 export function createPathOptions(
   { routes, directions, videos, type, hideUnlessVideo },
   { highlighted, selectedRoute, video }
 ) {
-  const isNoneSelected = !selectedRoute;
+  // const isNoneSelected = !selectedRoute;
   const isSelected = (routes ?? []).includes(selectedRoute?.name);
   const isHighlighted = (routes ?? []).includes(highlighted);
   const hasActiveVideo = videos?.includes(video);
   const isUnofficial = type === "unofficial";
   const isUncomfortable = type === "uncomfortable";
 
+  const unidirectionalColour = isSelected
+    ? UNIDIRECTIONAL_COLOUR_FULL
+    : UNIDIRECTIONAL_COLOUR_LIGHT;
+  const bidirectionalColour = isSelected
+    ? BIDIRECTIONAL_COLOUR_FULL
+    : BIDIRECTIONAL_COLOUR_LIGHT;
+
   const nonVideoColour =
-    directions?.length === 1 ? UNIDIRECTIONAL_COLOUR : BIDIRECTIONAL_COLOUR;
+    directions?.length === 1 ? unidirectionalColour : bidirectionalColour;
   const videoColour =
     directions?.length === 1
       ? VIDEO_UNIDIRECTIONAL_COLOUR
@@ -30,8 +39,10 @@ export function createPathOptions(
 
   return {
     color: hasActiveVideo ? videoColour : nonVideoColour,
-    weight: isHighlighted || hasActiveVideo ? WIDE_WEIGHT : NARROW_WEIGHT,
-    opacity: isSelected || isNoneSelected ? FULL_OPACITY : DIM_OPACITY,
+    weight: isHighlighted ? WIDE_WEIGHT : NARROW_WEIGHT,
+    // explicit opacity required to show hidden routes when video is selected
+    opacity: 100,
+    // opacity: isSelected || isNoneSelected ? FULL_OPACITY : DIM_OPACITY,
     dashArray: isUnofficial || isUncomfortable ? DASH_PATTERN : undefined,
   };
 }
