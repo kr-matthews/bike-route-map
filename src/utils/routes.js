a6e2ef214874e370931b76377da5326359843e81
Kevin Matthews <kvnrmatthews@gmail.com>
2023-10-24 19:47:36 -0700

    Fix video icon crash in legend map

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -2,6 +2,9 @@ import { ROUTES } from "../data/routes";
 
 export function hasVideo(segment, routeName) {
   const route = Object.values(ROUTES).find(({ name }) => name === routeName);
+  // route may be null for uses like the legend
+  if (!route) return false;
+
   const routeVideos = route.legs.flatMap(({ videos }) => Object.values(videos));
 
   return (segment.videos ?? []).some((video) =>
