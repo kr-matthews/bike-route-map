ee8cc22eb1b37191804dd09271c2c5e7cb93f6c1
Kevin Matthews <kvnrmatthews@gmail.com>
2023-12-04 18:48:05 -0800

    Display leg distance in UI

--- src/components/sidebar/Leg.jsx
+++ src/components/sidebar/Leg.jsx
@@ -3,17 +3,31 @@ import Video from "./Video";
 export default function Leg({ leg }) {
   const hasNoVideos = Object.entries(leg.videos).length === 0;
 
+  const distance = leg.distance ?? leg.distanceMax;
+  const distanceText = `Distance: ${(distance ?? 0).toFixed(1)} km`;
+
   return (
     <>
       {leg.name && (
-        <h3 style={{ textAlign: "center", marginBottom: "4px" }}>{leg.name}</h3>
+        <h3
+          style={{ textAlign: "center", marginBottom: "4px", marginTop: "2px" }}
+        >
+          {leg.name}
+        </h3>
+      )}
+      {distance && (
+        <div
+          style={{ paddingBottom: "4px", textAlign: "center", fontSize: "90%" }}
+        >
+          <span>{distanceText}</span>
+        </div>
       )}
       <div style={{ display: "flex", marginBottom: "4px" }}>
         {hasNoVideos && (
           <p style={{ paddingLeft: "1em" }}>No videos, yet. Come back later.</p>
         )}
         {Object.entries(leg.videos).map(([direction, video]) => (
-          <Video key={video} video={video} direction={direction} />
+          <Video key={video.id} video={video} direction={direction} />
         ))}
       </div>
     </>
