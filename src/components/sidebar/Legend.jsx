8554876b37003b367fd95dd66d26121f4d022f24
Kevin Matthews <kvnrmatthews@gmail.com>
2023-12-27 20:51:02 -0800

    Add colours to legend dropdown

--- src/components/sidebar/Legend.jsx
+++ src/components/sidebar/Legend.jsx
@@ -2,7 +2,18 @@ import { useState } from "react";
 import { MapContainer } from "react-leaflet";
 import Segment from "../map/Segment";
 import PanesAndTiles from "../map/PanesAndTiles";
-import { ZOOMED_IN_A_BIT } from "../../utils/constants";
+import {
+  COLOUR_CLOSED,
+  COLOUR_COMFORTABLE,
+  COLOUR_COMFORTABLE_ONE_WAY,
+  COLOUR_ELEVATED_BORDER,
+  COLOUR_OTHER,
+  COLOUR_PAINTED_ONE_WAY,
+  COLOUR_SHARED,
+  COLOUR_SHOULDER_ONE_WAY,
+  COLOUR_UNDERGROUND_BORDER,
+  ZOOMED_IN_A_BIT,
+} from "../../utils/constants";
 
 const intersection = [49.26208, -123.10495];
 
@@ -44,58 +55,68 @@ const mapZoom = ZOOMED_IN_A_BIT;
 const segmentTypes = [
   {
     name: "Comfortable",
+    colour: COLOUR_COMFORTABLE,
     description:
       "Local street, dedicated/protected lane/path, or mixed with pedestrians.",
     props: {},
   },
   {
     name: "Painted Lane",
+    colour: COLOUR_PAINTED_ONE_WAY,
     description:
       "Painted lane beside high-volume and/or high-speed traffic without significant physical protection. May or may not be directly beside parked cars.",
-    props: { type: "painted" },
+    props: { type: "painted", oneWay: "required" },
   },
   {
     name: "Shared Lane",
+    colour: COLOUR_SHARED,
     description: "Shared lane with high-volume and/or high-speed traffic.",
     props: { type: "shared" },
   },
   {
     name: "Highway Shoulder",
+    colour: COLOUR_SHOULDER_ONE_WAY,
     description: "Unprotected shoulder beside very high-speed traffic.",
-    props: { type: "shoulder" },
+    props: { type: "shoulder", oneWay: "required" },
   },
   {
     name: "Other",
+    colour: COLOUR_OTHER,
     description:
       "Separated from traffic but poor quality, such as a narrow sidewalk shared with pedestrians.",
     props: { type: "other" },
   },
   {
     name: "Elevated",
+    colour: COLOUR_ELEVATED_BORDER,
     description:
       "Bridge or other elevated surface. You can't directly connect to/from the roads/paths below.",
     props: { elevation: 1 },
   },
   {
-    name: "Underground",
+    name: "Underground/Covered",
+    colour: COLOUR_UNDERGROUND_BORDER,
     description:
-      "A tunnel. You can't directly connect to/from the roads/paths above.",
+      "A tunnel or other covered area. You can't directly connect to/from the roads/paths above.",
     props: { elevation: -1 },
   },
   {
     name: "One-way",
+    colour: COLOUR_COMFORTABLE_ONE_WAY,
     description:
       "Only one direction of travel is allowed. A lighter colour. Directional arrows only appear when sufficiently zoomed in.",
     props: { oneWay: "required" },
   },
   {
     name: "One-way Recommended",
+    colour: COLOUR_COMFORTABLE,
     description:
       "Only applies to comfortable. Both directions of travel are allowed, but typically only one is useful. Directional arrows only appear when sufficiently zoomed in.",
     props: { oneWay: "recommended" },
   },
   {
     name: "Closed",
+    colour: COLOUR_CLOSED,
     description: "For long-term construction or other reasons.",
     props: { isClosed: true },
   },
@@ -142,8 +163,12 @@ export default function Legend({ goBack }) {
           value={selectedTypeIndex}
           onChange={(e) => setSelectedTypeIndex(parseInt(e.target.value) || 0)}
         >
-          {segmentTypes.map(({ name }, index) => (
-            <option key={name} value={index}>
+          {segmentTypes.map(({ name, colour }, index) => (
+            <option
+              key={name}
+              value={index}
+              style={{ color: "White", backgroundColor: colour }}
+            >
               {name}
             </option>
           ))}
