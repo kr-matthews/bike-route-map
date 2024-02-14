059b4b6345c47f95113a37e7c5b9eebbbb448c8f
Kevin Matthews <kvnrmatthews@gmail.com>
2024-02-13 21:06:53 -0800

    Code type proportions in route background

--- src/components/sidebar/Routes.jsx
+++ src/components/sidebar/Routes.jsx
@@ -3,10 +3,15 @@ import { ROUTES } from "../../data/routes";
 import { Selections } from "../../App";
 import { Search } from "./Search";
 import { isSubsequence, removeWhiteSpaces } from "../../utils/strings";
-import { BLACK, DARK_GREEN, LIGHT_GREEN, WHITE } from "../../utils/colours";
+import { BLACK, WHITE } from "../../utils/colours";
 import { COLOUR_HIGHLIGHTED } from "../../utils/constants";
 import { VIEWS } from "./Sidebar";
 import Panel from "./Panel";
+import {
+  getWeightedRouteDistance,
+  getWeightedRouteDistanceOfType,
+} from "../../utils/routes";
+import { TYPE_TYPES } from "../../utils/segmentTypes";
 
 export default function Routes({ navigateTo }) {
   const [searchText, setSearchText] = useState("");
@@ -42,26 +47,20 @@ export default function Routes({ navigateTo }) {
   );
 }
 
-// !! route background colour
-// %s probably too much effort, so just 'main' colour; or uniform gradient with all 'relevant' colours
-// see https://stackoverflow.com/questions/19081355/how-can-apply-multiple-background-color-to-one-div
-
 function Route({ route }) {
   const { selectedRoute, setSelected, highlighted, setHighlighted } =
     useContext(Selections);
   const isHighlighted = highlighted === route.name;
   const isSelected = selectedRoute?.name === route.name;
 
+  const background = getBackgroundColor(route.name, isHighlighted);
+
   return (
     <div
       style={{
         textAlign: "center",
         width: "10em",
-        backgroundColor: isHighlighted
-          ? COLOUR_HIGHLIGHTED
-          : isSelected
-          ? DARK_GREEN
-          : LIGHT_GREEN,
+        background,
         borderRadius: "10px",
         padding: "6px 0",
         margin: "5px 0",
@@ -79,8 +78,54 @@ function Route({ route }) {
           color: isHighlighted ? BLACK : WHITE,
         }}
       >
-        {route.shortName ?? route.name}
+        {isSelected ? (
+          <i>
+            <u>{route.shortName ?? route.name}</u>
+          </i>
+        ) : (
+          route.shortName ?? route.name
+        )}
       </span>
     </div>
   );
 }
+
+// ex: linear-gradient(to right,  #9c9e9f 0%,#9c9e9f 50%,#33ccff 50%,#33ccff 100%);
+
+const getBackgroundColor = (routeName, isHighlighted) => {
+  if (isHighlighted) return COLOUR_HIGHLIGHTED;
+
+  const totalWeightedDistance = getWeightedRouteDistance(routeName);
+  const cumulativeWeightedDistances = TYPE_TYPES.reduce(
+    (acc, { key }) => [
+      ...acc,
+      acc[acc.length - 1] + getWeightedRouteDistanceOfType(routeName, key),
+    ],
+    [0]
+  );
+
+  const linearGradient =
+    "to right, " +
+    TYPE_TYPES.flatMap(({ colour }, index) =>
+      cumulativeWeightedDistances[index] ===
+      cumulativeWeightedDistances[index + 1]
+        ? []
+        : [
+            gradientPortion(
+              colour,
+              cumulativeWeightedDistances[index],
+              totalWeightedDistance
+            ),
+            gradientPortion(
+              colour,
+              cumulativeWeightedDistances[index + 1],
+              totalWeightedDistance
+            ),
+          ]
+    ).join(", ");
+
+  return `linear-gradient(${linearGradient})`;
+};
+
+const gradientPortion = (colour, weight, totalWeight) =>
+  `${colour} ${(100 * weight) / totalWeight}%`;
