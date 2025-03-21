3a3687d90a6b31e62bb50591dff719051e63c240
Kevin Matthews <kvnrmatthews@gmail.com>
2025-03-20 22:51:08 -0700

    Use DemoMap in App

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,22 @@ import useFilters from "./hooks/useFilters";
 import useSettings from "./hooks/useSettings";
 import { useHiddenParser } from "./hooks/useHiddenParser";
 import { WIDTH_BREAKPOINT } from "./utils/constants";
+import useDemo from "./hooks/useDemo";
+import DemoMap from "./components/map/DemoMap";
 
 export const SettingContext = createContext();
 export const FilterContext = createContext();
+export const DemoContext = createContext();
 
 export default function App() {
+  // which ref it is depends on which map is open (main or demo)
+  const [mapRef, setMapRef] = useState(null);
+
   const filters = useFilters();
   const settings = useSettings();
+  const demo = useDemo(mapRef);
 
   useHiddenParser();
-
-  const [mapRef, setMapRef] = useState(null);
 
   // only check on initial page load
   const [isScreenTooNarrow] = useState(window.innerWidth < WIDTH_BREAKPOINT);
@@ -24,18 +29,24 @@ export default function App() {
   return (
     <SettingContext.Provider value={settings}>
       <FilterContext.Provider value={filters}>
-        <div style={{ display: "flex" }}>
-          <MainMap
-            mapRef={mapRef}
-            setMapRef={setMapRef}
-            fullWidth={isScreenTooNarrow}
-          />
-          {isScreenTooNarrow ? (
-            <PhoneDialogs mapRef={mapRef} />
-          ) : (
-            <Sidebar mapRef={mapRef} />
-          )}
-        </div>
+        <DemoContext.Provider value={demo}>
+          <div style={{ display: "flex" }}>
+            {demo.isActive ? (
+              <DemoMap mapRef={mapRef} setMapRef={setMapRef} />
+            ) : (
+              <MainMap
+                mapRef={mapRef}
+                setMapRef={setMapRef}
+                fullWidth={isScreenTooNarrow}
+              />
+            )}
+            {isScreenTooNarrow ? (
+              <PhoneDialogs mapRef={mapRef} />
+            ) : (
+              <Sidebar mapRef={mapRef} />
+            )}
+          </div>
+        </DemoContext.Provider>
       </FilterContext.Provider>
     </SettingContext.Provider>
   );
