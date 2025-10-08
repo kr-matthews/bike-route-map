e03b95433a3f8b1fedd7644c548980bb6535076d
Kevin Matthews <kvnrmatthews@gmail.com>
2025-10-07 21:49:44 -0700

    Factor subcomponents out in Settings for clarity

--- src/components/sidebar/Settings.jsx
+++ src/components/sidebar/Settings.jsx
@@ -12,109 +12,122 @@ const getVideoSpeedName = (option) =>
   option === 1 ? "Real-time 1x" : "Time-lapse 4x (when available)";
 
 export default function Settings({ navigateTo }) {
-  const {
-    upgradesShown,
-    setUpgradesShown,
-    tileLayerKey,
-    setTileLayerKey,
-    defaultSpeed,
-    setDefaultSpeed,
-  } = useContext(SettingContext);
-
   return (
     <Panel name={VIEWS.settings.name} navigateTo={navigateTo}>
       <div style={{ overflowY: "scroll", overflow: "auto" }}>
-        {/* padding to match inherent padding on inputs, prevent scroll bar */}
-        <div style={{ padding: "4px", margin: "0.5em 0 1em 0" }}>
-          <b>Upgrade markers: </b>
-          {booleanOptions.map((option) => (
-            <label
-              key={option}
-              htmlFor="upgrades"
-              style={{
-                cursor: "pointer",
-                padding: "4px",
-              }}
-              onClick={() => setUpgradesShown(option === booleanOptions[0])}
-            >
-              <input
-                type="radio"
-                id={option}
-                name="upgrades"
-                style={{ cursor: "pointer" }}
-                checked={option === booleanOptions[upgradesShown ? 0 : 1]}
-                onChange={() => {}}
-              />
-              {option}
-            </label>
-          ))}
-        </div>
-
-        {/* padding to match inherent padding on inputs, prevent scroll bar */}
-        <div style={{ padding: "4px", margin: "0.5em 0 1em 0" }}>
-          <b>Background map: </b>
-          {Object.keys(TILE_LAYERS).map((tileLayerOption) => (
-            <label
-              key={tileLayerOption}
-              htmlFor="tiles"
-              style={{
-                cursor: "pointer",
-                padding: "4px",
-                textTransform: "capitalize",
-              }}
-              onClick={() => setTileLayerKey(tileLayerOption)}
-            >
-              <input
-                type="radio"
-                id={tileLayerOption.toString()}
-                name="tiles"
-                style={{ cursor: "pointer" }}
-                checked={tileLayerKey === tileLayerOption}
-                onChange={() => {}}
-              />
-              {tileLayerOption}
-            </label>
-          ))}
-        </div>
-
-        {/* padding to match inherent padding on inputs, prevent scroll bar */}
-        <div style={{ padding: "4px" }}>
-          <b>Default video speed: </b>
-          {videoSpeeds.map((videoSpeed) => {
-            const action = () => setDefaultSpeed(videoSpeed);
-            return (
-              <label
-                key={videoSpeed}
-                htmlFor="speed"
-                style={{
-                  cursor: "pointer",
-                  padding: "4px",
-                }}
-                onClick={action}
-              >
-                <input
-                  type="radio"
-                  id={videoSpeed.toString()}
-                  name="speed"
-                  style={{ cursor: "pointer" }}
-                  checked={defaultSpeed === videoSpeed}
-                  onChange={() => {}}
-                />
-                {getVideoSpeedName(videoSpeed)}
-              </label>
-            );
-          })}
-          <br />
-          {defaultSpeed === 4 && (
-            <div style={{ paddingTop: 5 }}>
-              <em>
-                Use the YouTube built-in controls to 2x speed, for an overall
-                effect of 8x.
-              </em>
-            </div>
-          )}
-        </div>
+        <UpgradeMarkerSetting />
+        <BackgroundMapSetting />
+        <DefaultSpeedSetting />
       </div>
     </Panel>
+  );
+}
+
+function UpgradeMarkerSetting() {
+  const { upgradesShown, setUpgradesShown } = useContext(SettingContext);
+
+  return (
+    // padding to match inherent padding on inputs, prevent scroll bar
+    <div style={{ padding: "4px", margin: "0.5em 0 1em 0" }}>
+      <b>Upgrade markers: </b>
+      {booleanOptions.map((option) => (
+        <label
+          key={option}
+          htmlFor="upgrades"
+          style={{
+            cursor: "pointer",
+            padding: "4px",
+          }}
+          onClick={() => setUpgradesShown(option === booleanOptions[0])}
+        >
+          <input
+            type="radio"
+            id={option}
+            name="upgrades"
+            style={{ cursor: "pointer" }}
+            checked={option === booleanOptions[upgradesShown ? 0 : 1]}
+            onChange={() => {}}
+          />
+          {option}
+        </label>
+      ))}
+    </div>
+  );
+}
+
+function BackgroundMapSetting() {
+  const { tileLayerKey, setTileLayerKey } = useContext(SettingContext);
+
+  return (
+    // padding to match inherent padding on inputs, prevent scroll bar
+    <div style={{ padding: "4px", margin: "0.5em 0 1em 0" }}>
+      <b>Background map: </b>
+      {Object.keys(TILE_LAYERS).map((tileLayerOption) => (
+        <label
+          key={tileLayerOption}
+          htmlFor="tiles"
+          style={{
+            cursor: "pointer",
+            padding: "4px",
+            textTransform: "capitalize",
+          }}
+          onClick={() => setTileLayerKey(tileLayerOption)}
+        >
+          <input
+            type="radio"
+            id={tileLayerOption.toString()}
+            name="tiles"
+            style={{ cursor: "pointer" }}
+            checked={tileLayerKey === tileLayerOption}
+            onChange={() => {}}
+          />
+          {tileLayerOption}
+        </label>
+      ))}
+    </div>
+  );
+}
+
+function DefaultSpeedSetting() {
+  const { defaultSpeed, setDefaultSpeed } = useContext(SettingContext);
+
+  return (
+    // padding to match inherent padding on inputs, prevent scroll bar
+    <div style={{ padding: "4px" }}>
+      <b>Default video speed: </b>
+      {videoSpeeds.map((videoSpeed) => {
+        const action = () => setDefaultSpeed(videoSpeed);
+        return (
+          <label
+            key={videoSpeed}
+            htmlFor="speed"
+            style={{
+              cursor: "pointer",
+              padding: "4px",
+            }}
+            onClick={action}
+          >
+            <input
+              type="radio"
+              id={videoSpeed.toString()}
+              name="speed"
+              style={{ cursor: "pointer" }}
+              checked={defaultSpeed === videoSpeed}
+              onChange={() => {}}
+            />
+            {getVideoSpeedName(videoSpeed)}
+          </label>
+        );
+      })}
+      <br />
+      {defaultSpeed === 4 && (
+        <div style={{ paddingTop: 5 }}>
+          <em>
+            Use the YouTube built-in controls to 2x speed, for an overall effect
+            of 8x.
+          </em>
+        </div>
+      )}
+    </div>
   );
 }
