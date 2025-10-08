843f811dd56c3b17536ff82a8fd4ff96e0541025
Kevin Matthews <kvnrmatthews@gmail.com>
2025-10-07 21:41:04 -0700

    Add upgrade videos

--- src/data/videos/index.js
+++ src/data/videos/index.js
@@ -1,3 +1,4 @@
 import { ROUTE_VIDEOS } from "./routes";
+import { UPGRADE_VIDEOS } from "./upgrades";
 
-export const VIDEOS = { routes: ROUTE_VIDEOS };
+export const VIDEOS = { routes: ROUTE_VIDEOS, upgrades: UPGRADE_VIDEOS };
