341a6199af7eecd92d5e8691677a8a3064991b05
Kevin Matthews <kvnrmatthews@gmail.com>
2025-10-10 22:11:24 -0700

    Allow markers to be centered

--- src/components/map/Marker.jsx
+++ src/components/map/Marker.jsx
@@ -1,11 +1,11 @@
 import { Marker as LeafletMarker, Tooltip } from "react-leaflet";
 import L from "leaflet";
 
-export default function Marker({ position, iconUrl, tooltip }) {
+export default function Marker({ position, iconUrl, tooltip, useCenter }) {
   const icon = L.icon({
     iconUrl,
     iconSize: [38, 45],
-    iconAnchor: [19, 40],
+    iconAnchor: useCenter ? undefined : [19, 40],
   });
 
   return (
