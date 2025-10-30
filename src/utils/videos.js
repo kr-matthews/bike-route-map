85968d2631f660a47d7aa9b84dde1ff9244bea30
Kevin Matthews <kvnrmatthews@gmail.com>
2025-10-29 21:19:16 -0700

    Minor cleanup

--- src/utils/videos.js
+++ src/utils/videos.js
@@ -4,6 +4,10 @@ import { ROUTE_VIDEOS } from "../data/videos/routes";
 import { UPGRADE_VIDEOS } from "../data/videos/upgrades";
 import { sumSegmentsLengths } from "./segments";
 
+// !! review file
+
+// route videos
+
 export function getRouteVideo(videoId) {
   return Object.values(ROUTE_VIDEOS).find(
     (video) => video.id.toLowerCase() === videoId?.toLowerCase()
@@ -43,12 +47,7 @@ const AUGMENTED_ROUTE_VIDEOS = Object.entries(ROUTE_VIDEOS).reduce(
 export const getAugmentedRouteVideo = (videoId) =>
   Object.values(AUGMENTED_ROUTE_VIDEOS).find((video) => video.id === videoId);
 
-export const getUpgradeVideo = (upgradeId) =>
-  Object.values(UPGRADE_VIDEOS).find((upgrade) => upgrade.id === upgradeId);
-
 export const routeVideoCount = Object.keys(ROUTE_VIDEOS).length;
-
-export const upgradeVideoCount = Object.keys(UPGRADE_VIDEOS).length;
 
 export const routeVideoCumulativeMinutes = Object.values(ROUTE_VIDEOS).reduce(
   (s, v) => s + v.minutes,
@@ -59,3 +58,10 @@ export const routeVideoCumulativeDistance = Object.values(ROUTE_VIDEOS).reduce(
   (s, v) => s + getRouteVideoDistance(v.id),
   0
 );
+
+// upgrade videos
+
+export const getUpgradeVideo = (upgradeId) =>
+  Object.values(UPGRADE_VIDEOS).find((upgrade) => upgrade.id === upgradeId);
+
+export const upgradeVideoCount = Object.keys(UPGRADE_VIDEOS).length;
