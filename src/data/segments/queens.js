8e0cd389cc9e1580b19a9f782d0b492d12e7e33c
Kevin Matthews <kvnrmatthews@gmail.com>
2026-09-18 17:10:48 -0700

    Extend queens rd data

--- src/data/segments/queens.js
+++ src/data/segments/queens.js
@@ -1,34 +1,103 @@
 import { MOSQUITO_CREEK_QUEENS_S } from "../intersections";
 import { ROUTES } from "../routes";
 
+const DEL_RIO_QUEENS_S = [49.33652, -123.0932];
 const DEL_RIO_QUEENS_N = [49.33663, -123.09317];
 
+const WOODBINE_QUEENS_NE = [49.33668, -123.09919];
+const WOODBINE_QUEENS_SE = [49.33661, -123.09893];
+const WOODBINE_QUEENS_W = [49.33665, -123.09938];
+const BEWICKE_QUEENS_E = [49.33658, -123.09098];
+const BEWICKE_QUEENS_SW = [49.33657, -123.0913];
+const BEWICKE_QUEENS_NW = [49.33662, -123.09109];
+
 export const QUEENS = [
+  // both
   {
-    routeNames: [ROUTES.queens.name],
-    description: "eb",
-    type: "dedicated",
-    oneWay: "required",
+    description: "queens & edgemont",
+    type: "shared",
+    undesignated: true,
     positions: [
-      [49.33661, -123.09893],
-      MOSQUITO_CREEK_QUEENS_S,
-      [49.33652, -123.0932],
+      [49.3375, -123.10192],
+      [49.33666, -123.10078],
+      WOODBINE_QUEENS_W,
+    ],
+  },
+  {
+    description: "queens, bewicke to jones",
+    type: "shared",
+    undesignated: true,
+    positions: [
+      BEWICKE_QUEENS_E,
+      [49.33656, -123.0881],
+      [49.33655, -123.08135],
     ],
   },
 
-  //
+  // eb
+  {
+    description: "eb start",
+    type: "shared",
+    oneWay: "recommended",
+    undesignated: true,
+    positions: [WOODBINE_QUEENS_W, WOODBINE_QUEENS_SE],
+  },
+  {
+    routeNames: [ROUTES.queens.name],
+    description: "eb protected",
+    type: "dedicated",
+    oneWay: "required",
+    positions: [WOODBINE_QUEENS_SE, MOSQUITO_CREEK_QUEENS_S, DEL_RIO_QUEENS_S],
+  },
+  {
+    routeNames: [ROUTES.queens.name],
+    description: "eb shared",
+    type: "shared",
+    oneWay: "recommended",
+    positions: [
+      DEL_RIO_QUEENS_S,
+      [49.33654, -123.09276],
+      [49.33657, -123.09212],
+      BEWICKE_QUEENS_SW,
+    ],
+  },
+  {
+    description: "eb end",
+    type: "shared",
+    oneWay: "recommended",
+    undesignated: true,
+    positions: [BEWICKE_QUEENS_SW, BEWICKE_QUEENS_E],
+  },
+
+  // wb
+  {
+    description: "wb start",
+    type: "shared",
+    oneWay: "recommended",
+    hideArrows: true,
+    undesignated: true,
+    positions: [BEWICKE_QUEENS_E, BEWICKE_QUEENS_NW],
+  },
   {
     routeNames: [ROUTES.queens.name],
     description: "wb painted",
     type: "painted",
     oneWay: "required",
-    positions: [[49.33662, -123.09109], DEL_RIO_QUEENS_N],
+    positions: [BEWICKE_QUEENS_NW, DEL_RIO_QUEENS_N],
   },
   {
     routeNames: [ROUTES.queens.name],
     description: "wb protected",
     type: "dedicated",
     oneWay: "required",
-    positions: [DEL_RIO_QUEENS_N, [49.33668, -123.09919]],
+    positions: [DEL_RIO_QUEENS_N, WOODBINE_QUEENS_NE],
+  },
+  {
+    description: "wb end",
+    type: "shared",
+    oneWay: "recommended",
+    hideArrows: true,
+    undesignated: true,
+    positions: [WOODBINE_QUEENS_NE, WOODBINE_QUEENS_W],
   },
 ];
